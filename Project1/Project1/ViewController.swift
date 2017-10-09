be22ee2e88d320b515a9d81ec1db156d277d245f
pcarderdev <31299967+pcarderdev@users.noreply.github.com>
2017-10-09 17:49:20 -0400

    Project1
    Beginning project1 on HackingWithSwift

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Patrick Carder on 10/5/17.
+//  Copyright © 2017 Patrick Carder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
